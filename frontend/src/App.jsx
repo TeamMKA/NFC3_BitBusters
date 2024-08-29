db206639741e6bcab5cd35f67415844343429bba
Mustafa <yusufmustufa@gmail.com>
2024-08-29 13:39:15 +0530

    Navbar basic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import {Route,Routes, BrowserRouter as Router} from 'react-router-dom'
 import Home from './pages/Home'
 import  './styles/app.scss';
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
    <Router>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,25 @@
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav>
+        <h1>Navbar</h1>
+
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/services">Services</a>
+            <a href="/contact">Contact</a>
+        </div>
+
+        <div>
+            <button>
+                Login
+            </button>
+            <button>
+                Register
+            </button>
+        </div>
+    </nav>
   )
 }
 
